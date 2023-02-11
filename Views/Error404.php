2f26d2485969327963d91398330de65da0e3b96f
Esteban Sanchez <easanrui@gmail.com>
2023-02-11 17:12:34 -0600

    Se incluye el Html y el Css de la pagina Error404.php

---
+++ Views/Error404.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PrimeraInfanciaCR</title>
+    <link rel="stylesheet" href="Dist/Css/Error404.css">
+    </head>
+    <body>
+        <div class="mars"></div>
+        <img src="Dist/Svg/404.svg" class="logo-404" />
+        <img src="Dist/Svg/meteor.svg" class="meteor" />
+        <p class="title">Oh no!!</p>
+        <p class="subtitle">
+            Digitaste una URL equivocada <br /> o estás haciendo una solicitud a una página que ya no existe.
+        </p>
+        <div align="center">
+            <a class="btn-back" href="Inicio.php">Volver al inicio</a>
+        </div>
+        <img src="Dist/Svg/astronaut.svg" class="astronaut" />
+        <img src="Dist/Svg/spaceship.svg" class="spaceship" />
+    </body>
+</html>
+
+
