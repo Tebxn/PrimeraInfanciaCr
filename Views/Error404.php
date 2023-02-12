af4a460a21c95f970509cfa494e5d4c0172bdd70
Esteban Sanchez <easanrui@gmail.com>
2023-02-11 23:09:38 -0600

    Me falto algo jeje

--- Views/Error404.php
+++ Views/Error404.php
@@ -4,12 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PrimeraInfanciaCR</title>
-    <link rel="stylesheet" href="Dist/Css/Error404.css">
+    <link rel="stylesheet" href="Css/Error404.css">
     </head>
     <body>
         <div class="mars"></div>
-        <img src="Dist/Svg/404.svg" class="logo-404" />
-        <img src="Dist/Svg/meteor.svg" class="meteor" />
+        <img src="Svg/404.svg" class="logo-404" />
+        <img src="Svg/meteor.svg" class="meteor" />
         <p class="title">Oh no!!</p>
         <p class="subtitle">
             Digitaste una URL equivocada <br /> o estás haciendo una solicitud a una página que ya no existe.
@@ -17,8 +17,8 @@
         <div align="center">
             <a class="btn-back" href="Inicio.php">Volver al inicio</a>
         </div>
-        <img src="Dist/Svg/astronaut.svg" class="astronaut" />
-        <img src="Dist/Svg/spaceship.svg" class="spaceship" />
+        <img src="Svg/astronaut.svg" class="astronaut" />
+        <img src="Svg/spaceship.svg" class="spaceship" />
     </body>
 </html>
 
